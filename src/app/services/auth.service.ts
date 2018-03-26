fb1da6157918de08cdd76b0a794691866892e4d9
Tommy Wong <xtommywongx@gmail.com>
2018-03-26 14:29:09 +0800

    finished facebook id

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
               }
 
   facebookLogin(access_token){
-    return this.http.post(`${environment.apiServer}/api/login/facebook`,{access_token:access_token})
+    return this.http.post(`${environment.apiServer}/api/auth/facebook`,{access_token:access_token})
       .subscribe((res: any)=>{
         this.token = res.token;
         localStorage.setItem('myToken',this.token);
@@ -51,6 +51,5 @@ export class AuthService {
     this.user = null;
     this.facebookAuthService.logOut();
     localStorage.removeItem('myToken');
-    this.router.navigate(['/login'])
   }
 }
