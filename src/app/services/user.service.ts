b82a5e60b33855cdd5ee16fb42fceb6697848a10
Bal Kumar Rai <preitam_rai@yahoo.com>
2018-04-17 10:53:51 +0800

    added progress bar for image upload

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpEventType, HttpResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs';
@@ -41,42 +41,29 @@ export class UserService {
 
   }
 
-  uploadPic(pic:File){
+  getPresignedUrl(pic:File){
     let options = this.createHeaders();
-   return this.http.get(`${environment.apiServer}/api/users/getPresignedUrl`, options)
-      .subscribe(data=> {
-          this.uploadToS3(data, pic);
-      })  
+   return this.http.get(`${environment.apiServer}/api/users/getPresignedUrl`, options).toPromise();
+  
   }
 
   uploadToS3(data, pic){
     return this.http.put(data['url'], pic, {
+              reportProgress: true,
+              observe: 'events',
               headers: {
                 'Content-Type': pic.type
-              }  }).subscribe(res => this.updateProfilePic(data));
+              } 
+             });
+             
   }
   updateProfilePic(data){
     let options = this.createHeaders();
 
     let img = 'https://s3.ap-northeast-2.amazonaws.com/building-sports-network/' + data['key'];
               
-    return this.http.put(`${environment.apiServer}/api/users/uploadPic`,{img} ,options)
-                .subscribe((data)=> {
-                  console.log('picture loaded');
-                  this.reloadProfile();
-                  if(data['success']){
-                    this.flashMessage.show(data['msg'],{
-                      cssClass: 'alert-success',
-                      timeout: 3000
-                    })
-                  } else {
-                    this.flashMessage.show(data['msg'], { 
-                      cssClass: 'alert-danger',
-                      timeout: 3000
-                    });
-                  }
-                });
-
+    return this.http.put(`${environment.apiServer}/api/users/uploadPic`,{img} ,options).toPromise();
+          
   }
 
   createHeaders(){
