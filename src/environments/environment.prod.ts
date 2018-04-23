57c0363cce511896d7b02fd7e4cbefd6a5d412c8
Tommy Wong <xtommywongx@gmail.com>
2018-04-23 11:26:41 +0800

    change environment production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   facebookAppId : "903864389793272",
-  apiServer: "https://api.bsn.host"
+  apiServer: "https://api.buildingsportsnetwork.tech"
 };
